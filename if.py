483be28bf1951259d98a6c1d0f973e83b74c1701
Nawa <arnawaugraaa@gmail.com>
2025-07-07 18:54:24 +0700

    Bring back the original robocafe and split the "if' material

---
+++ if.py
@@ -0,0 +1,9 @@
+#IF & ELSE
+
+#if is a keyword that checks if a condition is true or false (or we call it a boolean value).
+#else is a keyword that is used when the if condition is false.
+#so "if" condition is true, it will execute the code inside the if block.
+#If the condition is false, it will execute the code inside the else 
+#= is an operator to assign a value to a variable.
+#== is an operator to compare two values, and it returns True if they are equal, or False if they are not.
+
