401924ca6e564464b15a826d40482134ec346147
Nawa <arnawaugraaa@gmail.com>
2025-07-07 15:19:39 +0700

    first commit

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+#A number in Python is called an integer if it is a whole number, meaning it does not have a decimal point.
+
+#Defining variable here
+name = "Nolan"
+age = 21
+height = 164.57
+weight = 56.70
+
+
+print(f"{name} is {age} years old.")
+print(f"{name} is {height} cm tall.")
+print(f"{name} weighs {weight} kg. \n")
+#again, we use f-string to mix the variable with the string.
+#because the name is defined as a fixed string, it will always be "Nolan" unless we change the code.
+#because the age is defined as a fixed integer, it will always be 20 unless we change the code.
+#because the height is defined as a fixed float, it will always be 164.57 unless we change the code.
+#because the weight is defined as a fixed float, it will always be 56.70 unless we change the code.
+
+print("There are many types of data in Python, such as:")
+print(type(name))
+print(type(age))
+print(type(height))
+print()
+#type() is a function to tell you the type of the variable.
+#but it won't print anything to the console, so you need to use print() to see the result.
+#print() can also be used as a space between paragraphs, because it means you dont want to print anything, just a new line.
+
+#Now, let's do some math with the variables
+equation = height * age
+print(f"1. What is {height} times {age} equal to?")
+print(f"The answer is: {equation}")
+print(type(equation))
+#here, we're using equation as a variable to store the result of the multiplication.
+#python can also authomatically calculate the result of the multiplication, so you don't have to do it manually.
+#use asterisk * to multiply, slash / to divide, plus + to add, and minus - to subtract. ** to eskponentiate (pangkat).
+#you can also use parentheses () to group the operations, like this: (height * age) + 10.
---
+++ robocafe.py
@@ -0,0 +1,79 @@
+#Variable is a word that contains a value or data.
+#Input is a function that takes user input from the console.
+
+#Introduction of the AI assistant
+print("Hello, my name is Robocafe!\n" 
+      + "Your personal cafe AI assistant.\n")
+
+
+#Ask for the name of the user
+name = input("What is your name? ")
+#input() is a function that takes user input from the console.
+#name here is a variable that stores the user's name.
+#a word without quotes is a variable, which is contain value and data.
+#a word with quotes is a string, which is a value that represents text.
+
+#Greet the user nicely
+print(f"Hello {name}!, welcome to Le Cafe Julien.\n\n"
+      +"Thank you for coming in today.\n")
+#(f"aaaa") is an f-string, which allows you to mix variables and strings together.
+#{name} is a variable that being input by the user
+#in the f-string here, I used two newlines (\n\n), one for a new line after the greeting, and another one to create a space before the next sentence.
+
+#Define the menu as a variable
+menu = ("- Americano-style\n"
+        + "- Latte\n"
+        + "- Cappuccino\n"
+        + "- Espresso\n"
+        + "- Macchiato\n"
+        + "- Matcha\n"
+        + "- Hot Chocolate\n"
+        + "- Tea\n"
+        + "- Milktea\n"
+        + "- Flat-White\n"
+        + "- Cafe au Lait\n"
+        + "- Cold Brew\n"
+        + "- Affogato\n"
+        + "- Frappucino\n"
+        + "- Ristretto\n"
+        + "- Cortado\n"
+        + "- Irish Coffee\n"
+        + "- Doppio\n"
+        + "- Breve\n"
+        + "- Piccolo Latte\n"
+        + "- Red Eye\n"
+        + "- White Coffee\n"
+        + "- Nitro Cold Brew\n"
+        + "- Vietnamese Coffee\n"
+        + "- Lungo\n"
+        + "- Mocha\n")
+#\n is a newline character, which should be located at the end of each line but before the quotation mark.
+#\n can be multiplied like this: \n\n\n, which will create three new lines below.
+
+#Robocafe will ask the user what they would like to order, based on the menu definition
+print(f"Here is our menu:\n" + menu)
+print(name + ", what would you like to order today? ")
+order = input()
+print()
+#here, name and menu are already defined variables, so we can use them directly.
+#order is a variable that stores the user's order.
+#If there's no string in the input, just simply leave it empty, like this: input().
+#If you want to add a prompt, you can do it like this: input("What would you like to order? ").
+
+price = 8 #integer
+quantity = input("How many " + order + " would you like to order, " + name + "? \n") #string
+print()
+#quantity here is a variable that contains string and order variable.
+#i add the order variable inside, to automatically add the user's order to the string.
+
+#Robocafe notes the order and asks the user to wait
+total = price * int(quantity) #because quantity is a string, we need to convert it to an integer using int().
+print("Perfect " + name + "!, your " + quantity + " " + order + " will be ready in a moment.")
+print(f"That will be $" + str(total) + " in total.\n") #this (total) is still an integer, so we need to convert it to a string using str().
+#print here is a function that prints output to the console. () is what inside the print function itself.
+#"Perfect " is a string, a text. Do notice that i put a space after the word Perfect, so that the output will be more readable.
+#"!, your " is also a string, a text. I also put a space after the word your, so that the output have some space between your and order (variable).
+#order is what the user inputted.
+#" will be ready in a moment." is a string, a text. I also put a space after the word will, so that there's a space between order (variable) and will.
+#str() is a function that converts an integer to a string, so that it can be printed as text.
+#int() is a function that converts a string to an integer, so that it can be used in calculations.
